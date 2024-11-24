543058c534f3e45d8a8d5471392ae3a27bd27ff4
AngryRabbitChicken <Julian.Ferrero93@gmail.com>
2024-11-24 23:49:21 +0100

    Built check function for legal an illegal transmission values for the Nixie tube.

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,31 @@
 // put function declarations here:
 // TODO: The transfer only works with an array of 8 Bit integers, therefore wirte the check for an array of 8 bit integers.
 // each integer then corrseponds to one shift reg. Trying a little change.
-bool legal_spi_value(uint64_t);
+bool legal_spi_transmission(uint8_t *);
 void print_information(uint8_t *);
 
 uint32_t t_current, t_sys;
-uint64_t spi_value = 1;
 
-uint8_t nixie_buf[8];
+const uint8_t num_shift_regs = 8;
+const uint8_t digits_per_nixie = 10;
+uint8_t nixie_buf[num_shift_regs];
 
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("I successfully communicate!");
+
+  nixie_buf[0] = 0b00000000;
+  nixie_buf[1] = 0b00000110;
+  nixie_buf[2] = 0b00000000;
+  nixie_buf[3] = 0b00000000;
+  nixie_buf[4] = 0b00000001;
+  nixie_buf[5] = 0b00000000;
+  nixie_buf[6] = 0b00000001;
+  nixie_buf[7] = 0b00000100;
+
   t_current = millis();
-  Serial.println(sizeof(nixie_buf));
-  print_information(nixie_buf);
 }
 
 void loop()
@@ -29,31 +38,38 @@ void loop()
 
   if (t_sys >= t_current + 1000)
   {
-    /* t_current = t_sys;
-    if(legal_spi_value(spi_value, 10, 6)) Serial.println("Detected a legal SPI value!");
-    else Serial.println("Detected an illegal SPI value!"); */
+    t_current = t_sys;
+    if (legal_spi_transmission(nixie_buf))
+      Serial.println("Detected a legal SPI value!");
+    else
+      Serial.println("Detected an illegal SPI value!");
   }
 }
 
 // put function definitions here:
 
-bool legal_spi_value(uint64_t value, uint8_t tube_digits, uint8_t num_tubes)
+bool legal_spi_transmission(uint8_t spi_buf[num_shift_regs])
 {
-  uint8_t bits_on = 0;
-
-  for (int j = 0; j < num_tubes; j++)
+  uint8_t hi_bits = 0;
+  for (int j = 0; j < num_shift_regs; j++)
   {
-
-    for (int i = 0; i < tube_digits; i++)
+    for (int i = 0; i < 8; i++)
     {
+      if ((spi_buf[j] >> i & 1))
+      {
+        hi_bits++;
+      }
 
-      if ((value >> (i + j * tube_digits) & 1))
-        bits_on++;
+      if ((i + 1 + j * 8) % 10 == 0)
+      {
+        Serial.printf("I am at position %d.\n", i + 1 + j * 8);
+        if (hi_bits > 1)
+        {
+          return false;
+        }
+        hi_bits = 0;
+      }
     }
-    Serial.println(bits_on);
-    if (bits_on > 1)
-      return false;
-    bits_on = 0;
   }
   return true;
 }
@@ -61,6 +77,5 @@ bool legal_spi_value(uint64_t value, uint8_t tube_digits, uint8_t num_tubes)
 void print_information(uint8_t *arr)
 {
   uint8_t size = *(&arr + 1) - arr;
-  ;
   Serial.println(size);
 }
