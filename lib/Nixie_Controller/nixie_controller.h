4a1ccbb7ed7fa2813d6965d0971a9f7b6c6e1ba4
AngryRabbitChicken <Julian.Ferrero93@gmail.com>
2025-06-15 16:33:12 +0200

    Long time no see, back at it, no Idea what I'm doing

--- lib/Nixie_Controller/nixie_controller.h
+++ lib/Nixie_Controller/nixie_controller.h
@@ -6,21 +6,21 @@
 const uint8_t num_shift_registers = 8;
 
 class Nixie_Controller
-{       
-    public:
-        uint8_t num_nixies;
-        uint8_t digits_per_nixie;
-        uint8_t nixie_buf[num_shift_registers];
-        
-        bool errflg_illegal_digit = false;
+{
+public:
+    uint8_t num_nixies;
+    uint8_t digits_per_nixie;
+    uint8_t nixie_buf[num_shift_registers];
 
-        Nixie_Controller();
-        Nixie_Controller(uint8_t num_nixies, uint8_t digits_per_nixie);
+    bool errflg_illegal_digit = false;
 
-        void reset_nixie_buf();
-        void turn_on_digit(uint8_t digit);
-        void turn_on_tube(uint8_t tube, uint8_t digit);
-        bool check_buffer();
+    Nixie_Controller();
+    Nixie_Controller(uint8_t num_nixies, uint8_t digits_per_nixie);
+
+    void reset_nixie_buf();
+    void turn_on_digit(uint8_t digit);
+    void turn_on_tube(uint8_t tube, uint8_t digit);
+    bool check_buffer();
 };
 
 #endif
